dfc2ca1e64ea6cd3b6e918c80c8d86bb5e6049e2
WheatleyTheCore <alex.colwell@icloud.com>
2024-12-02 11:26:00 -0500

    partially completed audiomanager functions

--- DIYDJ/main/AudioManager.h
+++ DIYDJ/main/AudioManager.h
@@ -1,12 +1,32 @@
-struct ClothingState {
-  String head;
-  String top;
-  String bottom;
+
+#include "Arduino.h"
+#include "WiFiMulti.h"
+#include "Audio.h"
+#include "SD.h"
+#include "FS.h"
+
+// microSD Card Reader connections
+#define SD_CS 5
+#define SPI_MOSI 23
+#define SPI_MISO 19
+#define SPI_SCK 18
+
+struct AudioState
+{
+  String head_track;
+  String top_track;
+  String bottom_track;
 }
 
+class AudioManager
+{
+public:
+  struct AudioState nextState;
+  int initialize();
+  struct AudioState updateTracks(AudioState newAudioState);
+  void audioLoop();
 
-class AudioManager {
-  public:
-    struct ClothingState currentState;
-    struct ClothingState getCurrentTags();
+private:
+  Audio audio;
+  struct AudioState currentState; // should technically be private with getter and setter function
 }

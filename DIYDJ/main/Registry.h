164b9a6d1e33aa24662e4e687685386be6951325
Nico T <40475864+Nikoolaid@users.noreply.github.com>
2024-12-03 11:16:56 -0500

    Remove git words

--- DIYDJ/main/Registry.h
+++ DIYDJ/main/Registry.h
@@ -15,7 +15,6 @@ class Registry {
   public:
     int loadPairsFromSD(SD)
     struct RFIDTrackPair getPairByRFID(String RFID);
-<<<<<<< HEAD
     // struct RFIDTrackPair setRFIDTrackPair(String RFID, String track); // does not need to be implemented for demo
     // int RFIDTrackPair removePairbyRFID(String RFID); // does not need to be implemented for demo
     
@@ -24,10 +23,5 @@ class Registry {
     void writeFile(fs::FS &fs, const char * path, const char * message);
     void writeJSON(StaticJsonDocument<256> doc);
     const char* readJSON(char* key, StaticJsonDocument<256> doc);
-=======
-    struct RFIDTrackPair setRFIDTrackPair(String RFID, String track); // does not need to be implemented for demo
-    int RFIDTrackPair removePairbyRFID(String RFID); // does not need to be implemented for demo
-
->>>>>>> 393262f93c5d48f6c8136d5232e2d93e2aff81e0
 }
 
